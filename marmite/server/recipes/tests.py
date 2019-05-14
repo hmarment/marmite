d685f6d7db291c220b50cb4c47b5c7fc63d9a679
Henry Marment <henry@adsquare.com>
2019-05-14 21:40:08 +0200

    cleanup and format

--- marmite/server/recipes/tests.py
+++ marmite/server/recipes/tests.py
@@ -1,14 +1,22 @@
 # from django.test import TestCase
 import pytest
 
-from django.test.utils import setup_test_environment
 from django.test import Client
 from django.urls import reverse
 
 from .models import Recipe
 
 
-def create_recipe(name, short_description, type_, source, preparation_time, cooking_time, instructions, image):
+def create_recipe(
+    name,
+    short_description,
+    type_,
+    source,
+    preparation_time,
+    cooking_time,
+    instructions,
+    image,
+):
 
     return Recipe.objects.create(
         name=name,
@@ -18,7 +26,7 @@ def create_recipe(name, short_description, type_, source, preparation_time, cook
         preparation_time=preparation_time,
         cooking_time=cooking_time,
         instructions=instructions,
-        image=image
+        image=image,
     )
 
 
@@ -31,14 +39,14 @@ def client():
 def setup_test_recipe():
 
     recipe = create_recipe(
-        name='Recipes View - POST - Test Recipe',
-        short_description='How, now, brown, cow',
-        type_='self',
-        source='self',
+        name="Recipes View - POST - Test Recipe",
+        short_description="How, now, brown, cow",
+        type_="self",
+        source="self",
         preparation_time=30,
         cooking_time=45,
-        instructions='Take a can of baked beans, open it and pour into a saucepan.',
-        image='http://placekitten.com/g/200/300'
+        instructions="Take a can of baked beans, open it and pour into a saucepan.",
+        image="http://placekitten.com/g/200/300",
     )
 
     return recipe
@@ -48,49 +56,49 @@ def setup_test_recipe():
 def test_add_recipe(client):
 
     new_recipe_json = dict(
-        name='Recipes View - POST - Test Recipe',
-        short_description='How, now, brown, cow',
-        type='self',
-        source='self',
+        name="Recipes View - POST - Test Recipe",
+        short_description="How, now, brown, cow",
+        type="self",
+        source="self",
         preparation_time=30,
         cooking_time=45,
-        instructions='Take a can of baked beans, open it and pour into a saucepan.',
-        image='http://placekitten.com/g/200/300'
+        instructions="Take a can of baked beans, open it and pour into a saucepan.",
+        image="http://placekitten.com/g/200/300",
     )
-    response = client.post(reverse('recipes_view'), new_recipe_json)
+    response = client.post(reverse("recipes_view"), new_recipe_json)
 
     assert response.status_code == 201
 
     recipe = response.json()
-    
-    assert recipe.get('id') is not None
+
+    assert recipe.get("id") is not None
 
 
 @pytest.mark.django_db
 def test_list_recipes(client, setup_test_recipe):
 
     recipe = setup_test_recipe
-    response = client.get(reverse('recipes_view'))
-    
+    response = client.get(reverse("recipes_view"))
+
     assert response.status_code == 200
 
     recipes = response.json()
 
     assert len(recipes) == 1
-    assert recipes[0].get('id') == recipe.id
+    assert recipes[0].get("id") == recipe.id
 
 
 @pytest.mark.django_db
 def test_get_recipe_by_id(client, setup_test_recipe):
 
     recipe = setup_test_recipe
-    response = client.get(reverse('recipe_detail_view', args=[recipe.id]))
+    response = client.get(reverse("recipe_detail_view", args=[recipe.id]))
 
     assert response.status_code == 200
 
     recipe_json = response.json()
 
-    assert recipe_json.get('id') == recipe.id
+    assert recipe_json.get("id") == recipe.id
 
 
 @pytest.mark.django_db
@@ -98,18 +106,20 @@ def test_update_recipe(client, setup_test_recipe):
 
     recipe = setup_test_recipe
 
-    recipe_changes = dict(
-        name='Some new name',
-        preparation_time=60
-    )
+    recipe_changes = dict(name="Some new name", preparation_time=60)
 
-    response = client.put(reverse('recipe_detail_view', args=[recipe.id]), recipe_changes, content_type='application/json')
+    response = client.put(
+        reverse("recipe_detail_view", args=[recipe.id]),
+        recipe_changes,
+        content_type="application/json",
+    )
 
     assert response.status_code == 200
 
     updated_recipe_json = response.json()
 
-    assert updated_recipe_json.get('id') == recipe.id
-    assert updated_recipe_json.get('name') == recipe_changes.get('name')
-    assert updated_recipe_json.get('preparation_time') == recipe_changes.get('preparation_time')
-
+    assert updated_recipe_json.get("id") == recipe.id
+    assert updated_recipe_json.get("name") == recipe_changes.get("name")
+    assert updated_recipe_json.get("preparation_time") == recipe_changes.get(
+        "preparation_time"
+    )
